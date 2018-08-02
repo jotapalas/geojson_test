d8925edadf875f4b192d1c67944097ed382e7351
Juan Palacios <palaciosgalvanj@gmail.com>
2018-08-02 19:53:22 +0200

    Created basic database and loader

--- src/Controller/Activities/LoadAction.php
+++ src/Controller/Activities/LoadAction.php
@@ -4,24 +4,39 @@ namespace App\Controller\Activities;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\FileLocator;
+use App\Entity\Activity;
 
-class LoadAction
+class LoadAction extends Controller
 {
     /**
      * @Route("/activities/load", name="load_activities")
      */
     public function __invoke()
     {
-        return new Response(
-            '
-            <html>
-                <body>
-                    Load
-                </body>
-            </html>
-            '
-        );
+        $json = file_get_contents(__DIR__.'/../../../resources/madrid.json');
+        $json = json_decode($json);
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        foreach ($json as $activity) {
+            $activityInstance = new Activity (
+                $activity->name,
+                $activity->category,
+                $activity->location,
+                $activity->district,
+                $activity->hours_spent,
+                $activity->latlng[1],
+                $activity->latlng[0]
+            );
+            
+            $entityManager->persist($activityInstance);
+        }
+        
+        $entityManager->flush();
+        
+        return $this->render('activities/load.html.twig');
     }
 }
 
